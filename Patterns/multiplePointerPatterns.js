7fbb0bb36386e57340d1a1fa8d8529046baca836
syamjir <rsyamjir@gmail.com>
2024-09-29 11:02:06 +0530

    Add O(n) solution for multiple pointer patterns example

--- Patterns/multiplePointerPatterns.js
+++ Patterns/multiplePointerPatterns.js
@@ -6,7 +6,7 @@
 
 //approach-1 (O(n^2))
 
-function sumZero(arr) {
+function sumZero1(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       // nested loop
@@ -17,4 +17,28 @@ function sumZero(arr) {
   }
 }
 
-sumZero([-4, -3, -2, -1, 0, 1, 2, 5]); 
+sumZero1([-4, -3, -2, -1, 0, 1, 2, 5]);
+
+//approach-2 (O(n))
+
+function sumZero2(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+console.log(sumZero2([-4, -3, -2, -1, 0, 1, 2, 5])); //This function uses the two-pointer technique.
+//The array is assumed to be sorted. It uses two pointers (left and right), starting at the beginning and end of the array respectively.
+//The two pointers move towards each other:
+//If the sum of the two numbers is zero, the function returns that pair.
+//If the sum is positive, the right pointer moves left (to decrease the sum).
+//If the sum is negative, the left pointer moves right (to increase the sum).
